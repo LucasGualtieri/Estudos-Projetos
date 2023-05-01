11c6bc960ecddc9e64f63973e91cff06eacf5cfc
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-05-01 15:00:50 -0300

    .

--- C/EstudoString.c
+++ C/EstudoString.c
@@ -3,18 +3,21 @@
 int main() {
 
 	int idade;
+	printf("Digite sua idade: ");
 	scanf("%d", &idade);
 	printf("Idade: %d\n", idade);
 
-	String nome = trim(getstr(0));
+	pause(1);
+
+	String nome = trim(readString("Digite seu nome: "));
 	printf("Nome: %s\n", nome);
 	printf("Length: %d\n", (int)strlen(nome));
 
-	unsigned long CPFNumber = 41602264612;
-	String CPF = MaskCPF(CPFNumber);
+	CPF CPFNumber = 41602264612;
+	String cpf = MaskCPF(CPFNumber);
 
-	printf("CPF: %s\n", CPF);
-	printf("CPF Len: %d\n", (int)strlen(CPF));
+	printf("CPF: %s\n", cpf);
+	printf("CPF Len: %d\n", (int)strlen(cpf));
 
 	puts("\n******* | FIM DO PROGRAMA | *******\n");
 	return 0;
