b7d555621b982918371cdba89362d066b1f8e6c8
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-05-08 19:23:34 -0300

    .

--- Java/passagemDeParametros.java
+++ Java/passagemDeParametros.java
@@ -1,10 +1,14 @@
 public class passagemDeParametros {
 	public static void teste(int teste) {
-
+		teste = 20;
+		System.out.println(teste);
 	}
 
 	public static void main(String[] args) {
-
+		int idade = 0;
+		teste(idade);
+		System.out.println(idade);
 	}
-
 }
+
+// Não existe passagem por referencia AT ALL?????
