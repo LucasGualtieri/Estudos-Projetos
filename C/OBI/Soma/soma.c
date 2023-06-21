7a594970c35bbb2ce3d75635cdd6d1094cb15b75
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-06-21 03:40:11 -0300

    .

--- C/OBI/Soma/soma.c
+++ C/OBI/Soma/soma.c
@@ -3,32 +3,23 @@
 // By rangel and chatgpt
 
 int main() {
+
 	int arrayLength, sumTarget;
-
-	// printf("Digite o numero de quadrados que deseja: ");
-	scanf("%d", &arrayLength);
-
-	// printf("Digite o valor de soma desejado pra formar um retangulo: ");
-	scanf("%d", &sumTarget);
+	scanf("%d %d", &arrayLength, &sumTarget);
 
 	int array[arrayLength];
 
-	for (int i = 0; i < arrayLength; i++) {
-		// printf("Digite o numero do quadrado %d: ", i + 1);
-		scanf("%d", &array[i]);
-	}
+	for (int i = 0; i < arrayLength; i++) { scanf("%d", &array[i]); }
 
 	int qtdRetangulos = 0;
-
 	for (int i = 0; i < arrayLength; i++) {
 		int sum = 0;
 		for (int j = i; j < arrayLength; j++) {
-			sum += array[j];
-			if (sum == sumTarget) qtdRetangulos++;
+			if ((sum += array[j]) == sumTarget) qtdRetangulos++;
 		}
 	}
 
-	printf("Numero de retangulos formados: %d\n\n", qtdRetangulos);
+	printf("%d", qtdRetangulos);
 
 	return 0;
 }
