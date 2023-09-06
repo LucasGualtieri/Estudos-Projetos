4db5933b143cfbb254b68fc8e107bb1ff5d236b8
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-09-06 18:46:08 -0300

    Substr & Replaceall

---
+++ Java/SubstrNReplace.java
@@ -0,0 +1,50 @@
+// import TrabalhosPraticos.Lib;
+
+// clear && javac SubstrNReplace.java && java SubstrNReplace.java
+class SubstrNReplace {
+	
+	public static String substr(int start, int end, String str) {
+		if (end > str.length()) {
+			// throw new Exception("End index out of bounds");
+		}
+		
+		String aux = "";
+		for (int i = start; i < end; i++) {
+			aux += str.charAt(i);
+		}
+
+		return aux;
+	}
+
+	public static boolean isEqual(String str1, String str2) {
+		if (str1.length() != str2.length()) return false;
+
+		for (int i = 0; i < str1.length(); i++) {
+			if (str1.charAt(i) != str2.charAt(i)) return false;
+		}
+
+		return true;
+	}
+
+	public static String replaceAll(String baseStr, String newStr, String str) {
+		String aux = "";
+		int strLen = str.length();
+		int baseStrLen = baseStr.length();
+		for (int i = 0; i < strLen; i++) {
+			if (i < strLen - baseStrLen && isEqual(baseStr, substr(i, i + baseStrLen, str))) {
+				aux += newStr;
+				i += baseStrLen - 1; // Talvez não tenha o -1
+			} else {
+				aux += str.charAt(i);
+			}
+		}
+		return aux;
+	}
+
+	public static void main(String[] args) {
+		// Exemplo de uso String nome = replaceAll("()", " ", "Lucas()Gualtieri()Firace");
+		// Deve retornar Lucas Gualtieri Firace
+		String nome = replaceAll("()", " ", "Lucas()Gualtieri()Firace");
+		System.out.printf("Nome: %s\n", nome);
+	}
+}
