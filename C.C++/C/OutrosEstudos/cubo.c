6461566c0793ef3cc1bcd07f13c2d9f18629d719
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2023-09-19 20:29:47 -0300

    Alguns estudos

---
+++ C.C++/C/OutrosEstudos/cubo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+	int cubo[5][5][5];
+	for (int x = 0; x < 5; x++) {
+		for (int y = 0; y < 5; y++) {
+			for (int z = 0; z < 5; z++) {
+				cubo[x][y][z];
+			}
+		}
+	}
+}
