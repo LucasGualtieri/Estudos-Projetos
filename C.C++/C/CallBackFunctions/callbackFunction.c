99f681885dc28a550d27cd920484e376061cdd2a
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2024-02-19 19:41:28 -0300

    .

--- C.C++/C/CallBackFunctions/callbackFunction.c
+++ C.C++/C/CallBackFunctions/callbackFunction.c
@@ -1,18 +1,18 @@
-#include <biblioteca_c.h>
+#include <stdio.h>
 
-void printMsgPt(int idade, String nome) {
+void printMsgPt(int idade, char* nome) {
 	printf("Idade: %d, ", idade);
 	printf("Nome: %s\n", nome);
 }
 
-void printMsgEn(int age, String name) {
+void printMsgEn(int age, char* name) {
 	printf("Age: %d, ", age);
 	printf("Name: %s\n", name);
 }
 
-void callsFunctions(void (*function)(int, String)) {
+void callsFunctions(void (*function)(int, char*)) {
 	int idade = 20;
-	String nome = strdup("Lucas");
+	char* nome = strdup("Lucas");
 	function(idade, nome);
 }
 
