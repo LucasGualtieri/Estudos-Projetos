1e8be1f8ad95e4540524fd40dd3f4870ca16d75f
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2024-04-17 18:59:50 -0300

    Projeto financeiro, esboço

---
+++ C.C++/C++/ProjetoFinanceiro/Juros.cc
@@ -0,0 +1,13 @@
+// Dinheiro total
+// porcentagem a investir juros simples
+// porcentagem a investir juros composto
+// Uma forma de controlar a "resolução" do tempo, dia, semana, mes, ano
+/*
+
+	for (...) {
+		vai printar na tela no .csv os valores de x, y, z (tempo, (simples, composto))
+		print(juros(dinheiro))
+		print(jurosComp(dinheiro))
+	}
+
+*/
