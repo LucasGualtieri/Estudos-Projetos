30b8eef2bbb2272a94408365a7918ce4a8a28c90
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2024-06-10 13:32:47 -0300

    .

--- C.C++/C++/GenericMatrix/Islands.cc
+++ C.C++/C++/GenericMatrix/Islands.cc
@@ -1,33 +1,31 @@
 #include <iostream>
-#include <vector>
-#include <sstream>
 #include "Matrix.hpp"
 
 using namespace std;
 
 // clear && g++ Islands.cc && ./a.out
 
-template <typename T>
-void registerIsland(int i, int j, T& matrix) {
-	if (matrix.inBounds(i, j) && matrix[i][j] == 1) {
-		matrix[i][j] = 0;
-		registerIsland(i + 1, j, matrix);
-		registerIsland(i - 1, j, matrix);
-		registerIsland(i, j + 1, matrix);
-		registerIsland(i, j - 1, matrix);
+template <typename T, typename K>
+void registerIsland(int i, int j, K land, K water, T& matrix) {
+	if (matrix.inBounds(i, j) && matrix[i][j] == land) {
+		matrix[i][j] = water;
+		registerIsland(i + 1, j, land, water, matrix);
+		registerIsland(i - 1, j, land, water, matrix);
+		registerIsland(i, j + 1, land, water, matrix);
+		registerIsland(i, j - 1, land, water, matrix);
 	}
 }
 
-template <typename T>
-int islandCounter(T matrix) {
+template <typename T, typename K>
+int islandCounter(T matrix, K land, K water) {
 
 	int counter = 0;
 
 	for (int i = 0; i < matrix.height; i++) {
 		for (int j = 0; j < matrix.width; j++) {
-			if (matrix[i][j] == 1) {
+			if (matrix[i][j] == land) {
 				counter++;
-				registerIsland(i, j, matrix);
+				registerIsland(i, j, land, water, matrix);
 			}
 		}
 	}
@@ -47,5 +45,5 @@ int main() {
 	};
 
 	cout << m << endl;
-	cout << "There are " << islandCounter(m) << " islands." << endl;
+	cout << "There are " << islandCounter(m, 1, 0) << " islands." << endl;
 }
